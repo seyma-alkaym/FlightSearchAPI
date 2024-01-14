e1dc3b8c0fd294072af7ec1e929cdf3b155b7a1c
seyma <shaemaafadel1166s@gmail.com>
2024-01-14 17:16:55 +0300

    Add existsByCity and findByCity methods to AirportRepository.

--- src/main/java/com/example/flightsearchapi/repository/AirportRepository.java
+++ src/main/java/com/example/flightsearchapi/repository/AirportRepository.java
@@ -2,6 +2,13 @@ package com.example.flightsearchapi.repository;
 
 import com.example.flightsearchapi.entity.Airport;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface AirportRepository extends JpaRepository<Airport, Long> {
+    boolean existsByCity(String city);
+    @Query("SELECT a FROM Airport a WHERE a.city = :city")
+    Optional<Airport> findByCity(@Param("city") String city);
 }
