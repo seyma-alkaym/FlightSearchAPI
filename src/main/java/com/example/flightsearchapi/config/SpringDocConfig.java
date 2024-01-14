33908b21ea8bc2ae806650ee8df0bb736cad4d01
seyma <shaemaafadel1166s@gmail.com>
2024-01-14 17:38:02 +0300

    Add SpringDocConfig file (For swagger).

---
+++ src/main/java/com/example/flightsearchapi/config/SpringDocConfig.java
@@ -0,0 +1,33 @@
+package com.example.flightsearchapi.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class SpringDocConfig {
+
+    @Bean
+    public OpenAPI apiInfo() {
+
+        return new OpenAPI()
+                .info(
+                        new Info()
+                        .title("Backend Documentation")
+                        .version("1.0.0")
+                );
+    }
+
+    @Bean
+    public GroupedOpenApi httpApi() {
+        return GroupedOpenApi.builder()
+                .group("http")
+                .pathsToMatch("/**")
+                .build();
+    }
+}
