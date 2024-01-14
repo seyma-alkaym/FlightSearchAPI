c0f4ec4f0a8a1d743337c83dfba2db90ad21a404
seyma <shaemaafadel1166s@gmail.com>
2024-01-14 17:19:04 +0300

    Add findAllByArrivalAirportAndDepartureAirportAndDepartureDateTime and findAllByArrivalAirportAndDepartureAirportAndDepartureDateTimeAndReturnDateTime methods to FlightRepository file.

--- src/main/java/com/example/flightsearchapi/repository/FlightRepository.java
+++ src/main/java/com/example/flightsearchapi/repository/FlightRepository.java
@@ -1,8 +1,26 @@
 package com.example.flightsearchapi.repository;
 
 
+import com.example.flightsearchapi.entity.Airport;
 import com.example.flightsearchapi.entity.Flight;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface FlightRepository extends JpaRepository<Flight, Long> {
+
+    List<Flight> findAllByArrivalAirportAndDepartureAirportAndDepartureDateTime(
+            Airport arrivalAirport,
+            Airport departureAirport,
+            LocalDateTime departureDate
+    );
+
+    List<Flight> findAllByArrivalAirportAndDepartureAirportAndDepartureDateTimeAndReturnDateTime(
+            Airport arrivalAirport,
+            Airport departureAirport,
+            LocalDateTime departureDate,
+            LocalDateTime returnDate);
+
 }
